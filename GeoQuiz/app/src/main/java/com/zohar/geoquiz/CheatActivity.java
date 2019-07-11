6a8484b2e399ff0d0f53942c334d6d6ab7b49329
ZoharAndroid <1048132071@qq.com>
2019-07-11 23:45:24 +0800

    cheat布局完成

---
+++ GeoQuiz/app/src/main/java/com/zohar/geoquiz/CheatActivity.java
@@ -0,0 +1,13 @@
+package com.zohar.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
