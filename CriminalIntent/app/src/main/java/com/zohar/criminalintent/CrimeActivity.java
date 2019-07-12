f189d131472a197ac2a5ca5491ff95647ae5c1bb
ZoharAndroid <1048132071@qq.com>
2019-07-12 19:03:22 +0800

    add cirme project and 7th chapter end

--- CriminalIntent/app/src/main/java/com/zohar/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/zohar/criminalintent/CrimeActivity.java
@@ -2,7 +2,6 @@ package com.zohar.criminalintent;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
