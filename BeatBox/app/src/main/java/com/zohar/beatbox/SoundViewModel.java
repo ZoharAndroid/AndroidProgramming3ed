32e750a407d5a5e6a9c5cc7c9f85b6f94b3a355a
ZoharAndroid <1048132071@qq.com>
2019-07-17 03:38:38 +0800

    20章 完

--- BeatBox/app/src/main/java/com/zohar/beatbox/SoundViewModel.java
+++ BeatBox/app/src/main/java/com/zohar/beatbox/SoundViewModel.java
@@ -1,6 +1,9 @@
 package com.zohar.beatbox;
 
-public class SoundViewModel {
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class SoundViewModel extends BaseObservable {
 
     private Sound mSound;
     private BeatBox mBeatBox;
@@ -15,8 +18,10 @@ public class SoundViewModel {
 
     public void setSound(Sound sound) {
         mSound = sound;
+        notifyChange(); // 通知绑定类，viewmodel已经发生变化，绑定类因此会更新视图数据
     }
 
+    @Bindable
     public String getTitle(){
         return mSound.getName();
     }
