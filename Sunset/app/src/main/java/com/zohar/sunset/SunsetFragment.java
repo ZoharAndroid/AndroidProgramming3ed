cd92e05ad4ee59235cbdd7574153f40a80a0d41b
ZoharAndroid <1048132071@qq.com>
2019-07-19 01:51:34 +0800

    sunset project end

--- Sunset/app/src/main/java/com/zohar/sunset/SunsetFragment.java
+++ Sunset/app/src/main/java/com/zohar/sunset/SunsetFragment.java
@@ -1,5 +1,7 @@
 package com.zohar.sunset;
 
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,6 +10,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -15,11 +20,13 @@ public class SunsetFragment extends Fragment {
 
     private FrameLayout mSkyFrameLayout;
     private ImageView mSunImageView;
-    private  View mSceneView;
+    private View mSceneView;
 
-    public static Fragment newInstance(){
+    public static Fragment newInstance() {
         return new SunsetFragment();
     }
+
+    private boolean flag = false;
 
     @Nullable
     @Override
@@ -32,17 +39,66 @@ public class SunsetFragment extends Fragment {
         mSceneView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startAnimation();
+                flag = !flag;
+                if (flag) {
+                    startAnimation();
+                } else {
+                    upAnimation();
+                }
             }
         });
         return view;
     }
 
-    private void startAnimation(){
+    private void upAnimation() {
+        float sunYstart = mSkyFrameLayout.getHeight();
+        float sunYend = mSunImageView.getTop();
+
+        // 高度动画
+        ObjectAnimator hightAnimator = ObjectAnimator.ofFloat(mSunImageView, "y", sunYstart, sunYend).setDuration(3200);
+        hightAnimator.setInterpolator(new DecelerateInterpolator());
+
+        // 对sky黄昏到天亮
+        ObjectAnimator skyAnimator = ObjectAnimator.ofInt(mSkyFrameLayout, "backgroundColor", getResources().getColor(R.color.sunset_sky), getResources().getColor(R.color.blue_sky)).setDuration(3000);
+        skyAnimator.setEvaluator(new ArgbEvaluator());
+
+        // 夜晚到黄昏
+        ObjectAnimator nightAnimator = ObjectAnimator.ofInt(mSkyFrameLayout, "backgroundColor", getResources().getColor(R.color.night_sky), getResources().getColor(R.color.sunset_sky)).setDuration(1500);
+        nightAnimator.setEvaluator(new ArgbEvaluator());
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(hightAnimator).with(skyAnimator).after(nightAnimator);
+        animatorSet.start();
+
+    }
+
+    private void startAnimation() {
         float sunYstart = mSunImageView.getTop();
         float sunYend = mSkyFrameLayout.getHeight();
 
-        ObjectAnimator heighAnimator = ObjectAnimator.ofFloat(mSunImageView, "y", sunYstart, sunYend).setDuration(3200);
-        heighAnimator.start();
+        // 对sun进行设置
+        ObjectAnimator highAnimator = ObjectAnimator.ofFloat(mSunImageView, "y", sunYstart, sunYend).setDuration(3200);
+        // 添加加速插值器
+        highAnimator.setInterpolator(new AccelerateInterpolator());
+
+        // 对sky进行颜色处理
+        ObjectAnimator sunsetSkyAnimator = ObjectAnimator.ofInt(mSkyFrameLayout, "backgroundColor", getResources().getColor(R.color.blue_sky), getResources().getColor(R.color.sunset_sky))
+                .setDuration(3000);
+        // 设置颜色渐变程度
+        sunsetSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+        // 夜晚天空变化动画
+        ObjectAnimator nightSkyAnimator = ObjectAnimator.ofInt(mSkyFrameLayout, "backgroundColor", getResources().getColor(R.color.sunset_sky), getResources().getColor(R.color.night_sky)).setDuration(1500);
+        nightSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(highAnimator).with(sunsetSkyAnimator).before(nightSkyAnimator);
+        animatorSet.start();
+
+
+        highAnimator.start();
+        sunsetSkyAnimator.start();
+
+
     }
 }
