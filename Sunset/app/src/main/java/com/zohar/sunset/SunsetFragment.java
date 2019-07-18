86f4a2b3ca6cdc2d90b981920241d03a035ca957
ZoharAndroid <1048132071@qq.com>
2019-07-18 22:25:50 +0800

    sunset project

--- Sunset/app/src/main/java/com/zohar/sunset/SunsetFragment.java
+++ Sunset/app/src/main/java/com/zohar/sunset/SunsetFragment.java
@@ -1,5 +1,6 @@
 package com.zohar.sunset;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,8 +8,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 public class SunsetFragment extends Fragment {
+
+    private FrameLayout mSkyFrameLayout;
+    private ImageView mSunImageView;
+    private  View mSceneView;
 
     public static Fragment newInstance(){
         return new SunsetFragment();
@@ -17,6 +24,25 @@ public class SunsetFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+        mSceneView = view;
+        mSkyFrameLayout = view.findViewById(R.id.sky);
+        mSunImageView = view.findViewById(R.id.sun);
+
+        mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
+        return view;
+    }
+
+    private void startAnimation(){
+        float sunYstart = mSunImageView.getTop();
+        float sunYend = mSkyFrameLayout.getHeight();
+
+        ObjectAnimator heighAnimator = ObjectAnimator.ofFloat(mSunImageView, "y", sunYstart, sunYend).setDuration(3200);
+        heighAnimator.start();
     }
 }
